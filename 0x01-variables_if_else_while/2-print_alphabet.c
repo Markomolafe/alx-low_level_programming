c8d84a7bcc3812ca8c2f51000a6e75530f745c01
Markomolafe <komolafemark101@gmail.com>
2022-07-04 16:25:54 -0700

    work 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+putchar (a);
+putchar ('\n');
+return (0);
+}
